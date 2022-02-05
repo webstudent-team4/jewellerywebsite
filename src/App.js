e5f9166966602c8e18c06a784a348c15ec248285
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-05 17:11:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
+import Body from './body/Body';
 import { Routes, Route } from "react-router-dom"
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <div>
       <Routes>
         <Route path="/signup" element={ <SignUp/> } />
-        <Route path="/" element={ <SignIn/> } />
+        <Route path="/signin" element={ <SignIn/> } />
+        <Route path="/" element={ <Body/> } />
       </Routes>
       </div>
     </div>
