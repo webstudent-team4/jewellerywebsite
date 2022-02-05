56e4e67afe90a306144be1909b2c6c985702507b
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-05 17:01:40 +0530

    Create body.js

---
+++ src/body/body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../nav/Nav';
+import '../App.css';
+const Body = () => {
+  return (
+         <div>
+           <Nav/>
+             <body>
+              <p> this is a bady part</p>
+           </body>
+         </div>
+  );
+}
+
+export default Body;
