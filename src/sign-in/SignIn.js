aa1eb42656eabe67ecc9b4f0ef77d1028ca4f265
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-05 08:00:14 +0530

    Add files via upload

---
+++ src/sign-in/SignIn.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import {FaRegEyeSlash} from "react-icons/fa";
+import { ImUser } from "react-icons/im";
+import { Link } from "react-router-dom"
+import '../App.css';
+const SignIn = () => {
+  return (
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="https://i.ibb.co/PCKn1X2/login.jpg" alt="sing up image"/></figure>
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">Login</h2>
+                        <form method="POST" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="your_name"><ImUser/></label>
+                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass">  <FaRegEyeSlash/></label>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                            </div>
+                            <Link to="signup" className='signup-image-link'>Create account</Link>
+                           
+                           <div class="form-group form-button">
+                               <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                            </div>
+                        </form>
+                        <div class="social-login">
+                            <ul class="socials">
+                                <li><SocialIcon url="https://facebook.com/#" /></li>
+                                <li><SocialIcon url="https://google.com/#" /></li>
+                               <li><SocialIcon url="https://twitter.com/#" /></li> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  );
+}
+
+export default SignIn;
