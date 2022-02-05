000bb71c03b4d042307fbed17da3397d989e22d0
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-05 17:31:10 +0530

    Create Body.js

---
+++ src/body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../App.css';
+const Body = () => {
+  return (
+         <body>
+           this is body part
+           
+         </body>
+  );
+}
+
+export default Body;
