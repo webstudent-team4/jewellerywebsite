0f22c65c5f63f47e93096565e737f40629d3b06b
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-05 17:31:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
 import Body from './body/Body';
+import Nav from './nav/Nav';
 import { Routes, Route } from "react-router-dom"
 const App = () => {
   return (
     <div >
       <br/>  <br/> 
       <div>
+        <Nav/>
       <Routes>
         <Route path="/signup" element={ <SignUp/> } />
         <Route path="/signin" element={ <SignIn/> } />
