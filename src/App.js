449c7f2821b91b939cda4d10cd49a5b35b73a5cc
TheTruUchiha <75875192+TheTruUchiha@users.noreply.github.com>
2022-02-06 21:44:07 +0530

    added Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import SignIn from './sign-in/SignIn';
-import SignUp from './sign-up/SignUp';
-import { Routes, Route } from "react-router-dom"
+import React from "react";
+import "./App.css";
+import SignIn from "./sign-in/SignIn";
+import SignUp from "./sign-up/SignUp";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
 const App = () => {
   return (
-    <div >
-      <br/>  <br/> 
+    <div>
+      <br /> <br />
       <div>
-      <Routes>
-        <Route path="/signup" element={ <SignUp/> } />
-        <Route path="/" element={ <SignIn/> } />
-      </Routes>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/footer" element={<Footer />} />
+        </Routes>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/navbar/navbar-sub-components/EarringPop.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function EarringPop() {
+  return (
+    <div className="NavPopWrapper">
+      <div className="NavPopChildren">
+        <h1>BY CATEGORY</h1>
+        <hr />
+        <h1>Rings</h1>
+        <h1>Earrings</h1>
+        <h1>Pendants</h1>
+        <h1>Nose Pins</h1>
+      </div>
+      <div className="NavPopChildren">
+        <h1>POPULAR EARRING TYPE</h1>
+        <hr />
+        <h1>Stud</h1>
+        <h1>Drop</h1>
+        <h1>Hoops</h1>
+        <h1>Jhumkas</h1>
+        <h1>Sui Dhaga</h1>
+        <h1>Ear Cuffs</h1>
+      </div>
+      <div className="NavPopChildren">
+        <h1>POPULAR RING TYPES</h1>
+        <hr />
+        <h1>Daily Wear</h1>
+        <h1>Engagement</h1>
+        <h1>Solitaire</h1>
+        <h1>Cocktail</h1>
+        <h1>Office Wear</h1>
+        <h1>Men's</h1>
+        <h1>View All</h1>
+      </div>
+
+      <div className="NavPopChildren">
+        <h1>GOLD</h1>
+        <hr />
+        <h1>Rings</h1>
+        <h1>Earrings</h1>
+        <h1>Pendants</h1>
+        <h1>Gold Chains</h1>
+        <h1>Bracelets and Bangles</h1>
+        <h1>Nose Pins</h1>
+        <h1>Necklace</h1>
+        <h1>Mangalsutra</h1>
+        <h1>View All</h1>
+      </div>
+      <div className="NavPopImageWrapper">
+        <img
+          className="NavPopImage"
+          src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/earrings.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EarringPop;
