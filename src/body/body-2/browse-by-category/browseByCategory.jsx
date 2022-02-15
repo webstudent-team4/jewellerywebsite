31e1a8d6431d14e9577f2f00ffb1e0dd00c71819
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-15 10:51:03 +0530

    Update browseByCategory.jsx

--- src/body/body-2/browse-by-category/browseByCategory.jsx
+++ src/body/body-2/browse-by-category/browseByCategory.jsx
@@ -12,52 +12,52 @@ function BrowseByCategory() {
                 <div className="row">
                     <div className="category col-xl-6">
                         <div className="img-col">
-                            <a href="">
+                            <a href="#">
                                 <img className="img-responsive" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/rings.jpg" alt="" />
                             </a>
                         </div>
                         <div className="links-col">
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
                         </div>
                     </div>
                     <div className="category col-xl-6">
                         <div className="img-col">
-                            <a href="">
+                            <a href="#">
                                 <img className="img-responsive" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/earrings.jpg" alt="" />
                             </a>
                         </div>
                         <div className="links-col">
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
                         </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="category col-xl-6">
                         <div className="img-col">
-                            <a href="">
+                            <a href="#">
                                 <img className="img-responsive" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/pendants.jpg" alt="" />
                             </a>
                         </div>
                         <div className="links-col">
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
                         </div>
                     </div>
                     <div className="category col-xl-6">
                         <div className="img-col">
-                            <a href="">
+                            <a href="#">
                                 <img className="img-responsive" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/bangles-bracelets.jpg" alt="" />
                             </a>
                         </div>
                         <div className="links-col">
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
-                            <div><a href=""><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/upto10k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/10-25k.jpg" alt="" /></a></div>
+                            <div><a href="#"><img className="img-responsive" height="300" src="https://www.pcjeweller.com/public/uploads/exim//pcjw/category/above-25k.jpg" alt="" /></a></div>
                         </div>
                     </div>
                 </div>
