b5bc358449a17f339970bfccf8d71798f48b45b0
webstudent-team4 <98888201+webstudent-team4@users.noreply.github.com>
2022-02-15 11:30:33 +0530

    Update top-collections.jsx

--- src/body/body-2/top-collections/top-collections.jsx
+++ src/body/body-2/top-collections/top-collections.jsx
@@ -10,68 +10,68 @@ function TopCollections() {
             <div className="underline"></div>
             <div className="grid-container">
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/4/Dashavatar-pcj.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/4/enamel.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/4/amaila.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/Mirosa.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/4/hand-mangalsutra.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/expresso.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/bandhan.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/amor.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/Initials.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/2/twinning.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/inayat.jpg" alt="" />
                     </a>
                 </div>
                 <div className="grid-item">
-                    <a href="">
+                    <a href="/">
                         <img src="https://cf-cdn.pcjeweller.com/public/uploads/editor-images/collection2020/1/la-danza.jpg" alt="" />
                     </a>
                 </div>
             </div>
             <div className="view-more text-center">
-                <a href="" className="active viewmorelink">View all collections</a>
+                <a href="/" className="active viewmorelink">View all collections</a>
             </div>
         </div>
     );
